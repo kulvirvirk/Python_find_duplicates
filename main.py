672160482b6d8f6cbfcdb3d7852c641162b6ffc1
kulvirvirk <kvirkdev@gmail.com>
2020-02-27 07:32:54 +0000

    added code to find the duplicates

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #Excersise: print duplicates in a given list
+#please don't use sets for this excersise
 
 some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+duplicates_list = []
+
+for duplicate in some_list:
+  if some_list.count(duplicate) > 1:    #count() counts the number of times it sees the object
+    if duplicate not in duplicates_list:    #prevents printing double values
+      duplicates_list.append(duplicate)
+
+print(duplicates_list)
